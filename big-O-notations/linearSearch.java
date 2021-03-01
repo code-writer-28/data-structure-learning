a972ac6d7fbda977c096cfaef15fcc28fb80a3fe
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2021-03-01 08:11:10 +0530

    Create linearSearch.java

---
+++ big-O-notations/linearSearch.java
@@ -0,0 +1,30 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int num = sc.nextInt();
+	    int arr [] = new int [num];
+	    for(int i = 0; i < num; i++){
+	        arr[i] = sc.nextInt();
+	    }
+	    int value = sc.nextInt();
+	    
+	    System.out.println(linearSearch(arr,num,value));
+	}
+	
+	private static int linearSearch(int arr[], int num, int value){
+	    for(int i = 0; i < num ; i++){
+	        if(arr[i] == value)
+	            return 0;
+	    }
+	    return -1;
+	}
+	
+}
+
+
+solution : - https://ide.geeksforgeeks.org/6l6NIj38eC
