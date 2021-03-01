62f87036801db6487f8fb6a469509bcdf382dfc9
Shivam Thombre <68095774+code-writer-28@users.noreply.github.com>
2021-03-01 06:42:45 +0530

    Update sumofNaturalNumbers.java

--- analysis-of-algorithms/sumofNaturalNumbers.java
+++ analysis-of-algorithms/sumofNaturalNumbers.java
@@ -7,14 +7,28 @@ class GFG {
 	public static void main (String[] args) {
 	    Scanner sc = new Scanner(System.in);
 	    int number = sc.nextInt();
-		System.out.println("Sum of first " + number + " natural numbers is " +sumOfNaturalNumbers(number));
+		System.out.println("Sum of first " + number + " natural numbers is " +sumOfNaturalNumbersfirst(number));
+		System.out.println("Sum of first " + number + " natural numbers is " +sumOfNaturalNumbersSecond(number));
 	}
 	
-	private static int sumOfNaturalNumbers(int num){
+	private static int sumOfNaturalNumbersfirst(int num){
 	    int sum = 0;
-	    for(int i = 0; i < num; i++){
+	    for(int i = 1; i <= num; i++){
 	        sum  = sum + i;
 	    }
 	    return sum;
 	}
+	
+	private static int sumOfNaturalNumbersSecond(int num){
+	    return ((num * (num + 1))/2);
+	}
 }
+
+
+Solution :- 
+	https://ide.geeksforgeeks.org/QfhMhxk0Qr
+
+Answers :- 
+	Sum of first 10 natural numbers is 55
+	Sum of first 10 natural numbers is 55
+
